41da8888494e35f63c7e0834f6110220773559b8
Vinícius Atanásio <153241050+Vnny07@users.noreply.github.com>
2024-08-30 21:08:32 -0300

    Add files via upload

--- Projeto1.2/Login.py
+++ Projeto1.2/Login.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 from Menu import MenuApp
+from Usuarios import UsuariosApp
 
 # E-MAIL E SENHA PARA O LOGIN -> vinicius@email.com | 123456
 
@@ -9,13 +10,13 @@ class LoginApp:
     def __init__(self, master):
         self.master = master
         self.master.title("Login")
-        self.master.geometry("300x250")
+        self.master.geometry("300x210")
 
         label = tk.Label(master, text="Efetue o login", font=("Arial", 24, "bold"))
-        label.pack(pady=20)
+        label.pack(pady=10)
 
         frame = tk.Frame(master)
-        frame.pack(pady=20)
+        frame.pack(pady=10)
 
         self.label_email = tk.Label(frame, text="E-mail:")
         self.label_email.grid(row=0, column=0, padx=5, pady=5)
@@ -29,8 +30,19 @@ class LoginApp:
         self.entry_senha = tk.Entry(frame, show="*")
         self.entry_senha.grid(row=1, column=1, padx=5, pady=5)
 
-        self.login_button = tk.Button(master, text="Entrar", command=self.fazer_login)
-        self.login_button.pack(pady=10)
+        button_frame = tk.Frame(master)
+        button_frame.pack(pady=10)
+
+        self.login_button = tk.Button(button_frame, text="Entrar", command=self.fazer_login)
+        self.login_button.grid(row=0, column=0, padx=5)
+
+        self.cadastro_button = tk.Button(button_frame, text="Cadastrar", command=self.abrir_cadastro_usuarios)
+        self.cadastro_button.grid(row=0, column=1, padx=5)
+
+    def abrir_cadastro_usuarios(self):
+        janela_usuarios = tk.Toplevel()
+        app = UsuariosApp(janela_usuarios)
+        janela_usuarios.mainloop()
 
     def fazer_login(self):
         email = self.entry_email.get()
