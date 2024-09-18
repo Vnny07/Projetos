a3cf11e8876de406d9ca41c6c6fee856a1e8c496
Vinícius Atanásio <153241050+Vnny07@users.noreply.github.com>
2024-09-18 16:20:27 -0300

    Delete ProjetoIOT directory

--- ProjetoIOT/Limite.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-def janela_limite():
-    janela_limite = tk.Toplevel()
-    janela_limite.title("Limite de Consumo")
-    janela_limite.geometry("275x305")
-    janela_limite.resizable(True, False)
-    janela_limite.iconbitmap("ICO1.ico")
