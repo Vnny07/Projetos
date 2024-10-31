b1577f46bd7afe931ccdc28af8f26993dfd59abd
Vinícius Atanásio <153241050+Vnny07@users.noreply.github.com>
2024-10-30 21:30:37 -0300

    Add files via upload

--- SistemaV1/sistema/urls.py
+++ SistemaV1/sistema/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from . import views
-from django.views.generic import TemplateView
-from sistema.views import IndexTemplateView
-from sistema.views import TblUsuariosListView
 from sistema.views import TblUsuariosUpdateView
 from sistema.views import TblUsuariosDeleteView
 from sistema.views import TblUsuariosCreateView
@@ -29,19 +26,15 @@ app_name = 'website'
 
 urlpatterns = [
     path('', views.index, name='index'),
-
     path('', include('web.urls', namespace='web')),
-    path('admin/', admin.site.urls),
-
     path('', TemplateView.as_view(template_name="index.html")),
-
     path('', IndexTemplateView.as_view(), name='index'),
-
     path('usuarios/', TblUsuariosListView.as_view(), name='lista_usuarios')
-
     path('usuarios/<id>', TblUsuariosUpdateView.as_view(), name='atualiza_usuario'),
-
     path('usuarios/excluir/<pk>', TblUsuariosDeleteView.as_view(), name='deleta_usuario')
-
     path('usuarios/cadastrar/', TblUsuariosCreateView.as_view(), name='cadastra_usuario'),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
 ]
