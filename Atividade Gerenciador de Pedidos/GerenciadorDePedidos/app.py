359f82e7f7f948450a15dca14e5aad5c03d0b688
Vinícius Atanásio <153241050+Vnny07@users.noreply.github.com>
2025-03-11 21:46:55 -0300

    Add files via upload

---
+++ Atividade Gerenciador de Pedidos/GerenciadorDePedidos/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from models import db
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/clientes")
+def clientes():
+    return render_template("clientes.html")
+
+@app.route("/pedidos")
+def pedidos():
+    return render_template("pedidos.html")
+
+@app.route("/produtos")
+def produtos():
+    return render_template("produtos.html")
+
+@app.route("/novo_cliente")
+def novo_cliente():
+    return render_template("novo_cliente.html")
+
+@app.route("/editar_cliente")
+def editar_cliente():
+    return render_template("editar_cliente.html")
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    print("Banco de dados criado!")
+    app.run(debug=True)
---
+++ Atividade Gerenciador de Pedidos/GerenciadorDePedidos/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cliente(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    telefone = db.Column(db.String(20), nullable=False)
+
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.Text, nullable=True)
+    preco = db.Column(db.Numeric(10, 2), nullable=False)
+
+class Pedido(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_cliente = db.Column(db.Integer, db.ForeignKey("cliente.id"), nullable=False)
+    id_produto = db.Column(db.Integer, db.ForeignKey("produto.id"), nullable=False)
+    data_pedido = db.Column(db.DateTime, server_default=db.func.current_timestamp())
+
+    cliente = db.relationship("Cliente", backref="pedidos")
+    produto = db.relationship("Produto", backref="pedidos")
